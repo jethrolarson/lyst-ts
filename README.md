1b3695bfa4285ca7241e0b485abe53d3a0429c22
Jethro Larson <jethrolarson@gmail.com>
2021-04-25 15:34:55 -0700

    docs: adding more to the readme and adjusting styles of docco

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Lyst
+# lyst-ts
 
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
 Type-safe Immutable list for Typescript and JavaScript
 
+# Getting Started
+
+`npm install lyst-ts`
+
 [DOCS](http://www.jethrolarson.com/lyst/docs/src/lyst.html) (annotated source)
+
+# Why?
+
+This is primarilly a learning exercise for me and a way to try out code quality tools. If you find this useful, cool!
--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'src/**/*.ts',
-    ],
-    coverageReporters: ["text", "html"],
-    coverageThreshold: {
-      global: {
-        branches: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
-      }
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['text', 'html', 'json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
-    moduleFileExtensions: ["ts", "js"],
-    testRegex: "\\.spec\\.ts$",
-    transform: {
-      "^.+\\.ts$": "ts-jest"
-    },
-  };
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testRegex: '\\.spec\\.ts$',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+};
